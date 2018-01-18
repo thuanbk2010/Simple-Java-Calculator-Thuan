5b726da97af543e234fea1b67376701df1cebda8
thuantv <thuantv@bkav.com>
2018-01-18 19:44:18 +0700

    code for UI.java

--- src/java/study/SimpleJavaCaculator/UI.java
+++ src/java/study/SimpleJavaCaculator/UI.java
@@ -1,5 +1,19 @@
 package java.study.SimpleJavaCaculator;
 
-public class UI {
+public class UI implements ActionListener {
 
+	
+	private final JFrame  frame ;
+	private final JPanel  panel ;
+	private final JTextArea  text ;
+	private final JButton but[] , butAdd, butMinus, butMultiply, butDivide,
+	butEqual, butCancel, butSquareRoot, butSquare, butOneDevideBy, butCos,
+	butSin, butTan, butxPowofy, butLog,butRate ;
+	private final Calculator calc ;
+	
+	private final String[] buttonValue = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+	
+	public UI() {
+		
+	}
 }
